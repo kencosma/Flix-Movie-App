95a4db32075f6c4f1d0e3b1787239f6c04b4fb0e
Kennedi Cosma <kencosma2020@outlook.com>
2021-07-15 19:49:49 -0400

    Upgrades for part 2

---
+++ Flix Movie App/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flix Movie App
+//
+//  Created by Kennedi Cosma on 7/14/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
